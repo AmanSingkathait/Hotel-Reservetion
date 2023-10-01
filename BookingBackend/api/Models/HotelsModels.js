523498a7b2cee178acda61c7f7f9e3605feb8b14
Amansingh <kathaitaman05@gmail.com>
2023-10-01 23:45:59 +0530

    build the rooms route

--- BookingBackend/api/Models/HotelsModels.js
+++ BookingBackend/api/Models/HotelsModels.js
@@ -2,52 +2,52 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const HotelSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
     },
-    type:{
-        type:String,
-        required:true
+    type: {
+        type: String,
+        required: true
     },
-    city:{
-        type:String,
-        required:true
+    city: {
+        type: String,
+        required: true
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+        type: String,
+        required: true
     },
-    distance:{
-        type:String,
-        required:true
+    distance: {
+        type: String,
+        required: true
     },
-    photos:{
-        type:[String]
+    photos: {
+        type: [String]
     },
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    desc:{
-        type:String,
-        required:true
+    desc: {
+        type: String,
+        required: true
     },
-    rating:{
-        type:Number,
-        min:0,
-        max:5
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5
     },
-    rooms:{
-        type:[String]
+    rooms: {
+        type: [String],
     },
-    leastPrice:{
-        type:Number,
-        required:true
+    leastPrice: {
+        type: Number,
+        required: true
     },
-    featured:{
-        type:Boolean,
-        default:false
+    featured: {
+        type: Boolean,
+        default: false
     }
 })
 
--- BookingBackend/api/Routes/room.js
+++ BookingBackend/api/Routes/room.js
@@ -1,5 +1,72 @@
 const express = require('express');
 const route = express.Router();
+const Rooms = require('../Models/RoomModels');
+const Hotel = require('../Models/HotelsModels');
+const createError = require('../Utils/error');
+const { verifyAdmin, } = require('../Utils/VerifyToken')
 
 
+route.post("/:hotelid", verifyAdmin, async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    const newRoom = new Rooms(req.body);
+    try {
+        const savedRoom = await newRoom.save();
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $push: { rooms: savedRoom._id },
+            });
+        } catch (err) {
+            next(err);
+        }
+        res.status(200).json(savedRoom);
+    } catch (err) {
+        next(err);
+    }
+})
+route.put("/:id", verifyAdmin, async (req, res, next) => {
+    try {
+        const UpdateRoom = await Rooms.findByIdAndUpdate(req.params.id,
+            { $set: req.body }, { new: true })
+        res.status(200).json(UpdateRoom)
+    } catch (e) {
+        res.status(500).json(e);
+    }
+})
+
+route.delete("/:id/:hotelid", verifyAdmin, async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    try {
+        await Rooms.findByIdAndDelete(req.params.id);
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $pull: { rooms: req.params.id },
+            });
+        } catch (err) {
+            next(err);
+        }
+        res.status(200).json("Room has been deleted.");
+    } catch (err) {
+        next(err);
+    }
+})
+
+
+//GET
+route.get('/:id', verifyAdmin, async (req, res) => {
+    try {
+        const userRoom = await Rooms.findById(req.params.id);
+        res.status(200).json(userRoom)
+    } catch (e) {
+        res.status(500).json(e);
+    }
+})
+
+route.get('/', verifyAdmin, async (req, res, next) => {
+    try {
+        const userRooms = await Rooms.find();
+        res.status(200).json(userRooms)
+    } catch (e) {
+        next(e)
+    }
+})
 module.exports = route;
